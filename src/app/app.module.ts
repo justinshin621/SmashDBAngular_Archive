34f26216c01bc2da6d75c23f7f6154b9d7493424
Justin <justinshin@vt.edu>
2020-08-14 14:33:44 -0400

    Adding IGXAvatar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
-import {MaterialModule} from './material-module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { FighterComponent } from './fighter/fighter.component';
 import { MyGSPComponent } from './my-gsp/my-gsp.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { EditComponent } from './edit/edit.component';
-
 
 @NgModule({
   declarations: [
@@ -35,9 +34,10 @@ import { EditComponent } from './edit/edit.component';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
