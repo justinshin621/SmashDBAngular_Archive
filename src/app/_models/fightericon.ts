5e147d7c31be6c836bc6cc62886d934a4cdce733
Justin <justinshin@vt.edu>
2020-08-14 14:30:00 -0400

    Fighter Icons

---
+++ src/app/_models/fightericon.ts
@@ -0,0 +1,17 @@
+export enum FighterIcon {
+  kirby = 'https://www.elitegsp.com/flairs/6-min.png',
+  Fox = 'https://www.elitegsp.com/flairs/7-min.png',
+  Cloud = 'https://www.elitegsp.com/flairs/61-min.png',
+  Pikachu = 'https://www.elitegsp.com/flairs/8-min.png',
+  Mario = 'https://www.elitegsp.com/flairs/1-min.png',
+  Luigi = 'https://www.elitegsp.com/flairs/9-min.png',
+  Marth = 'https://www.elitegsp.com/flairs/21-min.png',
+  Roy = 'https://www.elitegsp.com/flairs/25-min.png',
+  Falco = 'https://www.elitegsp.com/flairs/20-min.png',
+  MKnight = 'https://www.elitegsp.com/flairs/27-min.png',
+  Peach = 'https://www.elitegsp.com/flairs/13-min.png',
+  Pichu = 'https://www.elitegsp.com/flairs/19-min.png',
+  CFalcon = 'https://www.elitegsp.com/flairs/11-min.png',
+  Ganon = 'https://www.elitegsp.com/flairs/23-min.png',
+  Yoshi = 'https://www.elitegsp.com/flairs/5-min.png'
+}
